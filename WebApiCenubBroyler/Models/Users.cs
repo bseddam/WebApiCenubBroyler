6eaa24263f14eaf2bfd48e0a8b4c96b6395162e8
bsedd <bsedd@SADDAM>
2020-12-21 12:04:06 +0400

    cc

---
+++ WebApiCenubBroyler/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiCenubBroyler.Models
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Sname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
